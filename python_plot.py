0699aec3dd5ace3c2d074cb7e318d71b2e049a3d
njhung <35144993+njhung@users.noreply.github.com>
2019-07-08 13:40:57 -0500

    Add files via upload

---
+++ python_plot.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = np.linspace(0,2*np.pi,100)
+f, ax = plt.subplots()
+ax.plot(x,np.sin(x))
+ax.set_xlabel('x')
+ax.set_ylabel('y')
